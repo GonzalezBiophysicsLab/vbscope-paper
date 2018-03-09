766ab7c0d770d09dd6578d2689303bc651ab61c3
byhuang2016 <byh2107@gmail.com>
2018-03-09 14:22:34 -0500

    update hist1D preferences

--- src/plots/hist_1d.py
+++ src/plots/hist_1d.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 default_prefs = {
-	'plotter_min_fret':-.5,
-	'plotter_max_fret':1.5,
-	'plotter_nbins_fret':41
+	'fret_min':-.5,
+	'fret_max':1.5,
+	'fret_nbins':41,
+
+	'hist_type':'stepfilled',
+	'hist_color':'steelblue',
+	'hist_edgecolor':'None',
+
+	'label_x_nticks':5
 }
 
 def plot(gui):
@@ -17,13 +23,15 @@ def plot(gui):
 		fpb = gui.data.get_plot_data()[0]
 
 		# plt.hist(f.flatten(),bins=181,range=(-.4,1.4),histtype='stepfilled',alpha=.8,normed=True)
-		popplot.ax[0].hist(fpb.flatten(),bins=popplot.prefs['plotter_nbins_fret'],range=(popplot.prefs['plotter_min_fret'],popplot.prefs['plotter_max_fret']),histtype='stepfilled',alpha=.8,density=True)
-
+		try:
+			popplot.ax[0].hist(fpb.flatten(),bins=popplot.prefs['fret_nbins'],range=(popplot.prefs['fret_min'],popplot.prefs['fret_max']),histtype=popplot.prefs['hist_type'],alpha=.8,density=True,color=popplot.prefs['hist_color'],edgecolor=popplot.prefs['hist_edgecolor'])
+		except:
+			popplot.ax[0].hist(fpb.flatten(),bins=popplot.prefs['fret_nbins'],range=(popplot.prefs['fret_min'],popplot.prefs['fret_max']),histtype='stepfilled',alpha=.8,density=True,color='steelblue')
 		if not gui.data.hmm_result is None:
 			r = gui.data.hmm_result
 			def norm(x,m,v):
 				return 1./np.sqrt(2.*np.pi*v)*np.exp(-.5/v*(x-m)**2.)
-			x = np.linspace(popplot.prefs['plotter_min_fret'],popplot.prefs['plotter_max_fret'],1001)
+			x = np.linspace(popplot.prefs['fret_min'],popplot.prefs['fret_max'],1001)
 			ppi = np.sum([r.gamma[i].sum(0) for i in range(len(r.gamma))],axis=0)
 			ppi /=ppi.sum()
 			v = r.b/r.a
@@ -34,7 +42,8 @@ def plot(gui):
 				popplot.ax[0].plot(x,y,color='k',lw=1,alpha=.8,ls='--')
 			popplot.ax[0].plot(x,tot,color='k',lw=2,alpha=.8)
 
-		popplot.ax[0].set_xlim(popplot.prefs['plotter_min_fret'],popplot.prefs['plotter_max_fret'])
+		popplot.ax[0].set_xlim(popplot.prefs['fret_min'],popplot.prefs['fret_max'])
+		popplot.ax[0].set_xticks(np.linspace(popplot.prefs['fret_min'],popplot.prefs['fret_max'],popplot.prefs['label_x_nticks']))
 		popplot.ax[0].set_xlabel(r'$\rm E_{\rm FRET}(t)$',fontsize=popplot.prefs['label_fontsize']/gui.plot.canvas.devicePixelRatio())
 		popplot.ax[0].set_ylabel('Probability',fontsize=popplot.prefs['label_fontsize']/gui.plot.canvas.devicePixelRatio())
 		for asp in ['top','bottom','left','right']:
