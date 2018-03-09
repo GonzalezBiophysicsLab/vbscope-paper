ffcba27c3cd32e99c4b442c5b87037c4d630cfa8
byhuang2016 <byh2107@gmail.com>
2018-03-09 13:17:44 -0500

    add script example

--- plotter.py
+++ plotter.py
@@ -1,3 +1,15 @@
 from src.ui import launch_plotter
 
 gui = launch_plotter(False)
+
+
+# # sample script
+# gui = launch_plotter(True)
+# gui.load_traces(filename='_traces.dat')
+# gui.load_classes(filename='_classes.dat')
+# gui.plot_hist1d()
+# popplot = gui.popout_plots['plot_hist1d'].ui
+# popplot.prefs['plotter_min_fret']=-.25
+# popplot.prefs['plotter_max_fret']=1.25
+# gui.plot_hist1d()
+# popplot.f.savefig('test.pdf')
