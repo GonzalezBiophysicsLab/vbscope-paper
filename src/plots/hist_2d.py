a2b818b7b07ec91a589c3cd5c37b48df0557801b
Colin Kinz-Thompson <ckinztho@gmail.com>
2018-03-09 12:51:49 -0500

    bridgets hist2d fixes

--- src/plots/hist_2d.py
+++ src/plots/hist_2d.py
@@ -4,25 +4,30 @@ import matplotlib.pyplot as plt
 
 default_prefs = {
 	'synchronize_start_flag':False,
-	'plotter_min_time':0,
-	'plotter_max_time':100,
-	'plotter_nbins_time':100,
-	'plotter_min_fret':-.25,
-	'plotter_max_fret':1.25,
-	'plotter_nbins_fret':81,
+	'time_min':0,
+	'time_max':100,
+	'time_nbins':100,
+	'fret_min':-.25,
+	'fret_max':1.25,
+	'fret_nbins':81,
 	'tau':.1,
 	'plotter_2d_syncpreframes':10,
 	'plotter_smoothx':.1,
 	'plotter_smoothy':.1,
-	'plotter_cmap':'rainbow',
-	'plotter_floorcolor':'lightgoldenrodyellow',
-	'plotter_floor':0.05,
-	'plotter_ceiling':0.95,
+	'color_cmap':'rainbow',
+	'color_floorcolor':'lightgoldenrodyellow',
+	'color_floor':0.05,
+	'color_ceiling':0.95,
 	'plotter_2d_normalizecolumn':False,
 	'plotter_timeshift':0,
 	'plotter_nbins_contour':200,
 
-	'plotter_colorbar_nticks':5
+	'label_colorbar_nticks':5,
+	'label_y_nticks':7,
+	'label_x_nticks':5,
+	'textbox_x':0.95,
+	'textbox_y':0.93,
+	'textbox_fontsize':8
 }
 
 def plot(gui):
@@ -67,16 +72,16 @@ def plot(gui):
 				fpb = np.array(oo)
 
 
-		dtmin = popplot.prefs['plotter_min_time']
-		dtmax = popplot.prefs['plotter_max_time']
+		dtmin = popplot.prefs['time_min']
+		dtmax = popplot.prefs['time_max']
 		if dtmax == -1:
 			dtmax = fpb.shape[1]
 		dt = np.arange(dtmin,dtmax)*popplot.prefs['tau']
 		ts = np.array([dt for _ in range(fpb.shape[0])])
 		fpb = fpb[:,dtmin:dtmax]
 		xcut = np.isfinite(fpb)
-		bt = (popplot.prefs['plotter_max_fret'] - popplot.prefs['plotter_min_fret']) / (popplot.prefs['plotter_nbins_fret'] + 1)
-		z,hx,hy = np.histogram2d(ts[xcut],fpb[xcut],bins = [popplot.prefs['plotter_nbins_time'],popplot.prefs['plotter_nbins_fret']+2],range=[[dt[0],dt[-1]],[popplot.prefs['plotter_min_fret']-bt,popplot.prefs['plotter_max_fret']+bt]])
+		bt = (popplot.prefs['fret_max'] - popplot.prefs['fret_min']) / (popplot.prefs['fret_nbins'] + 1)
+		z,hx,hy = np.histogram2d(ts[xcut],fpb[xcut],bins = [popplot.prefs['time_nbins'],popplot.prefs['fret_nbins']+2],range=[[dt[0],dt[-1]],[popplot.prefs['fret_min']-bt,popplot.prefs['fret_max']+bt]])
 		rx = hx[:-1]
 		ry = .5*(hy[1:]+hy[:-1])
 		x,y = np.meshgrid(rx,ry,indexing='ij')
@@ -85,22 +90,22 @@ def plot(gui):
 		z = gaussian_filter(z,(popplot.prefs['plotter_smoothx'],popplot.prefs['plotter_smoothy']))
 
 		# cm = plt.cm.rainbow
-		# vmin = popplot.prefs['plotter_floor']
+		# vmin = popplot.prefs['color_floor']
 		# cm.set_under('w')
 		# if vmin <= 1e-300:
 		# 	vmin = z.min()
 		# pc = plt.pcolor(y.T,x.T,z.T,cmap=cm,vmin=vmin,edgecolors='face')
 		try:
-			cm = plt.cm.__dict__[popplot.prefs['plotter_cmap']]
+			cm = plt.cm.__dict__[popplot.prefs['color_cmap']]
 		except:
 			cm = plt.cm.rainbow
 		try:
-			cm.set_under(popplot.prefs['plotter_floorcolor'])
+			cm.set_under(popplot.prefs['color_floorcolor'])
 		except:
 			cm.set_under('w')
 
-		vmin = popplot.prefs['plotter_floor']
-		vmax = popplot.prefs['plotter_ceiling']
+		vmin = popplot.prefs['color_floor']
+		vmax = popplot.prefs['color_ceiling']
 
 		if popplot.prefs['plotter_2d_normalizecolumn'] == 'True':
 			z /= np.nanmax(z,axis=1)[:,None]
@@ -129,7 +134,12 @@ def plot(gui):
 				popplot.f.axes[1].cla()
 				cb = popplot.f.colorbar(pc,cax=popplot.f.axes[1])
 
-			cbticks = np.linspace(popplot.prefs['plotter_floor'],popplot.prefs['plotter_ceiling'],popplot.prefs['plotter_colorbar_nticks'])
+			# cbticks = np.linspace(popplot.prefs['color_floor'],popplot.prefs['color_ceiling'],popplot.prefs['label_colorbar_nticks'])
+			cbticks = np.linspace(0.,1.,popplot.prefs['label_colorbar_nticks'])
+			cbticks = cbticks[cbticks > popplot.prefs['color_floor']]
+			cbticks = cbticks[cbticks < popplot.prefs['color_ceiling']]
+			cbticks = np.append(popplot.prefs['color_floor'], cbticks)
+			cbticks = np.append(cbticks, popplot.prefs['color_ceiling'])
 			cb.set_ticks(cbticks)
 			cb.set_ticklabels(["%.2f"%(cbt) for cbt in cbticks])
 
@@ -146,9 +156,11 @@ def plot(gui):
 		popplot.f.subplots_adjust(left=.05+popplot.prefs['label_padding'],bottom=.05+popplot.prefs['label_padding'],top=.95,right=.95)
 
 		popplot.ax[0].set_xlim(rx.min()-popplot.prefs['plotter_timeshift'],rx.max()-popplot.prefs['plotter_timeshift'])
-		popplot.ax[0].set_ylim(popplot.prefs['plotter_min_fret'],popplot.prefs['plotter_max_fret'])
+		popplot.ax[0].set_ylim(popplot.prefs['fret_min'],popplot.prefs['fret_max'])
+		popplot.ax[0].set_yticks(np.linspace(popplot.prefs['fret_min'],popplot.prefs['fret_max'],popplot.prefs['label_y_nticks']))
+		popplot.ax[0].set_xticks(np.linspace(popplot.prefs['time_min']*popplot.prefs['tau']-popplot.prefs['plotter_timeshift'],popplot.prefs['time_max']*popplot.prefs['tau']-popplot.prefs['plotter_timeshift'],popplot.prefs['label_x_nticks']))
 		popplot.ax[0].set_xlabel('Time (s)',fontsize=popplot.prefs['label_fontsize']/gui.plot.canvas.devicePixelRatio())
 		popplot.ax[0].set_ylabel(r'$\rm E_{\rm FRET}(t)$',fontsize=popplot.prefs['label_fontsize']/gui.plot.canvas.devicePixelRatio())
 		bbox_props = dict(boxstyle="square", fc="w", alpha=1.0,lw=1./gui.plot.canvas.devicePixelRatio())
-		popplot.ax[0].annotate('n = %d'%(fpb.shape[0]),xy=(.95,.93),xycoords='axes fraction',ha='right',color='k',bbox=bbox_props,fontsize=popplot.prefs['label_ticksize']/gui.plot.canvas.devicePixelRatio())
+		popplot.ax[0].annotate('n = %d'%(fpb.shape[0]),xy=(popplot.prefs['textbox_x'],popplot.prefs['textbox_y']),xycoords='axes fraction',ha='right',color='k',bbox=bbox_props,fontsize=popplot.prefs['textbox_fontsize']/gui.plot.canvas.devicePixelRatio())
 		popplot.canvas.draw()
